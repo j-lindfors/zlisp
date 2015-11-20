9e8bdffbffcc922b2983cfee5e1fdfe932746f15
Joakim Lindfors <joakim@lindfors.co>
2015-11-20 13:21:31 +0100

    Fixed typo

--- parsing.c
+++ parsing.c
@@ -37,7 +37,7 @@ int main(int argc, char** argv) {
 
     /* Define them with the following Language */
     mpca_lang(MPCA_LANG_DEFAULT,
-        "
+        "                                                       \
         number      :   /-?[0-9]+/ ;                            \
         operator    :   '+' | '-' | '*' | '/' ;                 \
         expr        :   <number> | '(' <operator> <expr>+ ')';  \

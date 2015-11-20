a31003d262e7f01e10dba17fed49d08c4a1fe678
Joakim Lindfors <joakim@lindfors.co>
2015-11-20 21:07:53 +0100

    Added some error handling

--- parsing.c
+++ parsing.c
@@ -27,30 +27,96 @@ void add_history(char* unused) {}
 #include <histedit.h> 
 #endif
 
-/* Use operator string to see which operation to perform */
-long eval_op(long x, char* op, long y) {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    if (strcmp(op, "%") == 0) { return x % y; }
-    if (strcmp(op, "^") == 0) { return pow(x,y); }
-    
-    return 0;
+/* Create enumeration of possible error types */
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+/* Create enumeration of possible lval types */
+enum { LVAL_NUM, LVAL_ERR };
+
+/* Declare new lval struct */
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+/* Create new number type lval */
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
 }
 
-long eval(mpc_ast_t* t) {
-    
-    /* If tagged as number return it directly. */
-    if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+/* Create a new error type lval */
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+/* Print an "lval" */
+void lval_print(lval v) {
+    switch (v.type) {
+        /* In the case the type is a number print it
+         * Then 'break' out of the switch */
+        case LVAL_NUM: printf("%li", v.num); break;
+
+        /* In the case the type is an error */
+        case LVAL_ERR:
+            /* Check what type of error it is and print it */
+            if (v.err == LERR_DIV_ZERO) {
+                printf("Error: Division by zero!");
+            }
+            if (v.err == LERR_BAD_OP) {
+                printf("Error: Invalid operator!");
+            }
+            if (v.err == LERR_BAD_NUM) {
+                printf("Error: Invalid number!");
+            }
+            break;
+    }
+}
+
+/* Print an "lval" followed by a newline */
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
+
+/* Use operator string to see which operation to perform */
+lval eval_op(lval x, char* op, lval y) {
+
+    /* If either value is an error return it */
+    if (x.type == LVAL_ERR) { return x; }
+    if (x.type == LVAL_ERR) { return y; }
+
+    /* Otherwise do maths on the number values */
+    if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+    if (strcmp(op, "%") == 0) { return lval_num(x.num % y.num); }
+    if (strcmp(op, "^") == 0) { return lval_num(pow(x.num,y.num)); }
+    if (strcmp(op, "/") == 0) { 
+        /* If second operand is zero, return error */
+        return y.num == 0
+            ? lval_err(LERR_DIV_ZERO)
+            : lval_num(x.num / y.num);
     }
 
-    /* The operator is always second child. */
-    char* op = t->children[1]->contents;
+    return lval_err(LERR_BAD_OP);
+}
 
-    /* We store the third child in 'x' */
-    long x = eval(t->children[2]);
+lval eval(mpc_ast_t* t) {
+    
+    if(strstr(t->tag, "number")) {
+        /* Check if there is some error in conversion */
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+    }
+
+    char* op = t->children[1]->contents;  
+    lval x = eval(t->children[2]);
 
     /* Iterate the remaining children and combining. */
     int i = 3;
@@ -79,7 +145,7 @@ int main(int argc, char** argv) {
         ",
         Number, Operator, Expr, Zlisp);
     
-    puts("zLisp version 0.0.0.1");
+    puts("zLisp version 0.0.0.4");
     puts("Press Ctrl+c to Exit\n");
 
     while (1) {
@@ -91,8 +157,8 @@ int main(int argc, char** argv) {
         /* Attempt to parse the user input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Zlisp, &r)) {
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
         } else {
             /* Otherwise print the error */

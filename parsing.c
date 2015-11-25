86278014683cde4417bfda27308227d1f2eb522f
Joakim Lindfors <joakim@lindfors.co>
2015-11-25 18:37:50 +0100

    renamed func to builtin

--- parsing.c
+++ parsing.c
@@ -38,10 +38,14 @@ typedef lval*(*lbuiltin)(lenv*, lval*);
 
 struct lval {
   int type;
+
+  /* Basic */
   long num;
   char* err;
   char* sym;
-  lbuiltin fun;
+
+  /* Function */
+  lbuiltin builtin;
   int count;
   lval** cell;
 };
@@ -87,7 +91,7 @@ lval* lval_sym(char* s) {
 lval* lval_fun(lbuiltin func) {
   lval* v = malloc(sizeof(lval));
   v->type = LVAL_FUN;
-  v->fun = func;
+  v->builtin = func;
   return v;
 }
 
@@ -134,7 +138,7 @@ lval* lval_copy(lval* v) {
   switch (v->type) {
     
     /* Copy Functions and Numbers Directly */
-    case LVAL_FUN: x->fun = v->fun; break;
+    case LVAL_FUN: x->builtin = v->builtin; break;
     case LVAL_NUM: x->num = v->num; break;
     
     /* Copy Strings using malloc and strcpy */
@@ -507,7 +511,7 @@ lval* lval_eval_sexpr(lenv* e, lval* v) {
   }
   
   /* If so call function to get result */
-  lval* result = f->fun(e, v);
+  lval* result = f->builtin(e, v);
   lval_del(f);
   return result;
 }
